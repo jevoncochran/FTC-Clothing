1801de3732e83355602aca1ca7ecf5eab1dcf3ea
Jevon Cochran <jevon.cochran@gmail.com>
2021-01-01 16:53:19 -0800

    Nav shop drawer

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,80 +4,115 @@ import ftcLogoBlack from "../../assets/images/nav/ftc-logo-black.png";
 import { NavLink } from "react-router-dom";
 import { AiOutlineShoppingCart, AiOutlineSearch } from "react-icons/ai";
 import { IoCloseSharp } from "react-icons/io5";
+import { AiOutlineCloseSquare } from "react-icons/ai";
+import { navAisles } from "./navAisles";
+import NavAisle from "./NavAisle";
 
 const Nav = (props) => {
   const [searchActive, setSearchActive] = useState(false);
+  const [shopActive, setShopActive] = useState(false);
 
   return (
-    <div className="nav">
-      <div className="nav-logo-div">
-        <NavLink to="/">
-          <img
-            className="nav-logo"
-            src={ftcLogoBlack}
-            alt="For the Culture Clothing logo"
-          />
-        </NavLink>
-      </div>
-      <div className="nav-pages-div">
-        <NavLink className="nav-link" to="/">
-          Home
-        </NavLink>
-        <NavLink className="nav-link" to="/shop">
-          Shop
-        </NavLink>
-        <NavLink className="nav-link" to="/designers">
-          Designers
-        </NavLink>
-        <NavLink className="nav-link" to="/contact">
-          Contact
-        </NavLink>
-      </div>
-      <div className="nav-actions-div">
-        <div
-          className="nav-actions-search-div"
-          style={{ border: searchActive ? "1px solid black" : "none" }}
-        >
-          <div className="nav-action-icon">
-            <IoCloseSharp
-              style={{
-                borderRight: searchActive ? "1px solid black" : "none",
-                height: "100%",
-                display: searchActive ? "block" : "none",
-              }}
-              onClick={() => setSearchActive(false)}
+    <div
+      className="nav"
+      style={{
+        position: shopActive ? "absolute" : "relative",
+        zIndex: shopActive ? 999 : 0,
+      }}
+    >
+      <div className="nav-top">
+        <div className="nav-logo-div">
+          <NavLink to="/">
+            <img
+              className="nav-logo"
+              src={ftcLogoBlack}
+              alt="For the Culture Clothing logo"
             />
-          </div>
-          <input
-            className="nav-actions-search-input"
-            type="text"
-            name="search_query"
-            placeholder="Enter query here"
-            style={{ display: searchActive ? "block" : "none" }}
-          />
+          </NavLink>
+        </div>
+        <div className="nav-pages-div">
+          <NavLink className="nav-link" to="/">
+            Home
+          </NavLink>
+          <p className="nav-link" onClick={() => setShopActive(true)}>
+            Shop
+          </p>
+          <NavLink className="nav-link" to="/designers">
+            Designers
+          </NavLink>
+          <NavLink className="nav-link" to="/contact">
+            Contact
+          </NavLink>
+        </div>
+        <div className="nav-actions-div">
           <div
-            className="nav-action-icon"
-            style={{
-              //   border: "1px dashed black",
-              //   width: "80%",
-              display: "flex",
-              justifyContent: "flex-end",
-              alignItems: "center",
-            }}
-            onClick={() => setSearchActive(true)}
+            className="nav-actions-search-div"
+            style={{ border: searchActive ? "1px solid black" : "none" }}
           >
-            <AiOutlineSearch
-              style={{
-                // border: "1px dashed black",
-                height: "100%",
-                borderLeft: searchActive ? "1px solid black" : "none",
-              }}
+            <div className="nav-action-icon">
+              <IoCloseSharp
+                style={{
+                  borderRight: searchActive ? "1px solid black" : "none",
+                  height: "100%",
+                  display: searchActive ? "block" : "none",
+                }}
+                onClick={() => setSearchActive(false)}
+              />
+            </div>
+            <input
+              className="nav-actions-search-input"
+              type="text"
+              name="search_query"
+              placeholder="Enter query here"
+              style={{ display: searchActive ? "block" : "none" }}
             />
+            <div
+              className="nav-action-icon"
+              style={{
+                //   border: "1px dashed black",
+                //   width: "80%",
+                display: "flex",
+                justifyContent: "flex-end",
+                alignItems: "center",
+              }}
+              onClick={() => setSearchActive(true)}
+            >
+              <AiOutlineSearch
+                style={{
+                  // border: "1px dashed black",
+                  height: "100%",
+                  borderLeft: searchActive ? "1px solid black" : "none",
+                }}
+              />
+            </div>
+          </div>
+
+          <div className="nav-action-icon">
+            <AiOutlineShoppingCart />
           </div>
         </div>
-
-        <div className="nav-action-icon">
-          <AiOutlineShoppingCart />
+      </div>
+      <div
+        className="nav-bottom"
+        style={{ display: shopActive ? "flex" : "none" }}
+      >
+        <div className="nav-bottom-aisles">
+          {navAisles.map((aisle) => {
+            return (
+              <NavAisle
+                key={aisle.aisle}
+                image={aisle.image}
+                aisle={aisle.aisle}
+                backgroundPosition={aisle.background_position}
+              />
+            );
+          })}
+        </div>
+        <div
+          className="nav-bottom-close-icon"
+          onClick={() => setShopActive(false)}
+        >
+          <AiOutlineCloseSquare />
         </div>
       </div>
     </div>
---
+++ client/src/components/Nav/navAisles.js
@@ -0,0 +1,29 @@
+import women from "../../assets/images/nav/nav_aisle_women.jpg";
+import men from "../../assets/images/nav/nav_aisle_men.jpg";
+import kids from "../../assets/images/nav/nav_aisle_kids.jpg";
+import masks from "../../assets/images/nav/nav_aisle_masks.jpg";
+import caps from "../../assets/images/nav/nav_aisle_caps.jpg";
+
+export const navAisles = [
+  {
+    image: women,
+    aisle: "WOMEN",
+    background_position: "0 20%",
+  },
+  {
+    image: men,
+    aisle: "MEN",
+  },
+  {
+    image: kids,
+    aisle: "KIDS",
+  },
+  {
+    image: masks,
+    aisle: "MASKS",
+  },
+  {
+    image: caps,
+    aisle: "CAPS",
+  },
+];
